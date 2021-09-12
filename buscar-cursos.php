a165a5375815d9d74610d351dcfb602bfdcd1343
Andre Sousa <andre.sousa40@etec.sp.gov.br>
2021-09-12 18:03:33 -0300

    Adicionando tag

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -6,6 +6,8 @@ use Andredss99\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
+//
+
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
 
